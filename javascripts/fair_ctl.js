aa11fb070838ab0cdadf7197120756f24219d091
Ties Westendorp <tieswestendorp@gmail.com>
2020-10-09 15:47:52 +0200

    Pass 'fairness' correctly

--- javascripts/fair_ctl.js
+++ javascripts/fair_ctl.js
@@ -13,13 +13,13 @@ class FairCTL {
     if (fairness.size == 0) return ctl.EX(p, trans)
     return bdd.andN(fairness.map(cond => ctl.EX(ctl.EU(p, bdd.and(p, cond), trans), trans)))
   }
-  static EG(p, fairness, trans) { return ctl.gfp(u => bdd.and(p, FairCTL.EX(u, trans))) }
-  static EF(p, fairness, trans) { return ctl.lfp(u => bdd.or(p,  FairCTL.EX(u, trans))) }
-  static EU(p, q, trans) { return ctl.lfp(u => bdd.or(p, bdd.and(q, FairCTL.EX(u, trans)))) }
-  static AX(p,    trans) { return bdd.not(FairCTL.EX(bdd.not(p), trans)) }
-  static AG(p,    trans) { return ctl.gfp(u => bdd.and(p, FairCTL.AX(u, trans))) }
-  static AF(p,    trans) { return ctl.lfp(u => bdd.or(p,  FairCTL.AX(u, trans))) }
-  static AU(p, q, trans) { return ctl.lfp(u => bdd.or(p, bdd.and(q, FairCTL.AX(u, trans)))) }
+  static EG(p, fairness, trans) { return ctl.gfp(u => bdd.and(p, FairCTL.EX(u, fairness, trans))) }
+  static EF(p, fairness, trans) { return ctl.lfp(u => bdd.or(p,  FairCTL.EX(u, fairness, trans))) }
+  static EU(p, q, trans) { return ctl.lfp(u => bdd.or(p, bdd.and(q, FairCTL.EX(u, fairness, trans)))) }
+  static AX(p,    trans) { return bdd.not(FairCTL.EX(bdd.not(p), fairness, trans)) }
+  static AG(p,    trans) { return ctl.gfp(u => bdd.and(p, FairCTL.AX(u, fairness, trans))) }
+  static AF(p,    trans) { return ctl.lfp(u => bdd.or(p,  FairCTL.AX(u, fairness, trans))) }
+  static AU(p, q, trans) { return ctl.lfp(u => bdd.or(p, bdd.and(q, FairCTL.AX(u, fairness, trans)))) }
 }
 FairCTL.reset()
 
