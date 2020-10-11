3f367a71c49366e5302d027fad6b436233ee0e42
Ties Westendorp <tieswestendorp@gmail.com>
2020-10-11 16:19:53 +0200

    Use spread operator in philosophers example

--- examples/computation-tree-logic/3-dining-philosophers.js
+++ examples/computation-tree-logic/3-dining-philosophers.js
@@ -2,7 +2,6 @@
 
 const bdd = require('../../lib/binary-decision-diagrams')
 const ctl = require('../../lib/computation-tree-logic')
-const fair_ctl = require('../../lib/fair-computation-tree-logic')
 
 const n = 3
 
@@ -29,40 +28,37 @@ for(let i = 0; i < n; i++) {
 
   // if possible, philosopher i grabs left utensil
   transitions.push(bdd.andN([
-    bdd.andN(indices_except_i.map(j => bdd.eql(left_utensils[j][0],  left_utensils[j][1]))),
-    bdd.andN(         indices.map(j => bdd.eql(right_utensils[j][0], right_utensils[j][1]))),
-    bdd.andN(         indices.map(j => bdd.eql(eating[j][0],         eating[j][1]))),
-
+    bdd.andN(indices_except_i.map(j => bdd.eql(...left_utensils[j]))),
+    bdd.andN(         indices.map(j => bdd.eql(...right_utensils[j]))),
+    bdd.andN(         indices.map(j => bdd.eql(...eating[j]))),
     bdd.not(left_utensils[i][0]), bdd.not(right_utensils[left_philosopher][0]),
     left_utensils[i][1]
   ]))
 
   // if possible, philosopher i grabs right utensil
   transitions.push(bdd.andN([
-    bdd.andN(         indices.map(j => bdd.eql(left_utensils[j][0],  left_utensils[j][1]))),
-    bdd.andN(indices_except_i.map(j => bdd.eql(right_utensils[j][0], right_utensils[j][1]))),
-    bdd.andN(         indices.map(j => bdd.eql(eating[j][0],         eating[j][1]))),
-
+    bdd.andN(         indices.map(j => bdd.eql(...left_utensils[j]))),
+    bdd.andN(indices_except_i.map(j => bdd.eql(...right_utensils[j]))),
+    bdd.andN(         indices.map(j => bdd.eql(...eating[j]))),
     bdd.not(right_utensils[i][0]), bdd.not(left_utensils[right_philosopher][0]),
     right_utensils[i][1]
   ]))
 
   // if philosopher i has both utensils, start eating
   transitions.push(bdd.andN([
-    bdd.andN(indices_except_i.map(j => bdd.eql(left_utensils[j][0],  left_utensils[j][1]))),
-    bdd.andN(indices_except_i.map(j => bdd.eql(right_utensils[j][0], right_utensils[j][1]))),
-    bdd.andN(indices_except_i.map(j => bdd.eql(eating[j][0],         eating[j][1]))),
-
-    left_utensils[i][0], right_utensils[i][0], bdd.not(eating[i][0]),
-    left_utensils[i][1], right_utensils[i][1], eating[i][1]
+    bdd.andN(indices_except_i.map(j => bdd.eql(...left_utensils[j]))),
+    bdd.andN(indices_except_i.map(j => bdd.eql(...right_utensils[j]))),
+    bdd.andN(indices_except_i.map(j => bdd.eql(...eating[j]))),
+    bdd.and(...left_utensils[i]),
+    bdd.and(...right_utensils[i])
+    bdd.not(eating[i][0]), eating[i][1]
   ]))
 
   // if philosopher i is eating, he may finish eating and return utensils
   transitions.push(bdd.andN([
-    bdd.andN(indices_except_i.map(j => bdd.eql(left_utensils[j][0],  left_utensils[j][1]))),
-    bdd.andN(indices_except_i.map(j => bdd.eql(right_utensils[j][0], right_utensils[j][1]))),
-    bdd.andN(indices_except_i.map(j => bdd.eql(eating[j][0],         eating[j][1]))),
-
+    bdd.andN(indices_except_i.map(j => bdd.eql(...left_utensils[j]))),
+    bdd.andN(indices_except_i.map(j => bdd.eql(...right_utensils[j]))),
+    bdd.andN(indices_except_i.map(j => bdd.eql(...eating[j]))),
     left_utensils[i][0],          right_utensils[i][0],          eating[i][0],
     bdd.not(left_utensils[i][1]), bdd.not(right_utensils[i][1]), bdd.not(eating[i][1])
   ]))
@@ -80,7 +76,6 @@ const reachable = ctl.reachable(initial, transition)
 const deadlocks = ctl.deadlock(transition)
 const reachable_deadlocks = bdd.and(reachable, deadlocks)
 
-console.warn("Number of reachable states: ",   reachable.numberOfSatisfyingAssignments(3*n))
+console.warn("Number of reachable states:",    reachable.numberOfSatisfyingAssignments(3*n))
 console.warn("Number of reachable deadlocks:", reachable_deadlocks.numberOfSatisfyingAssignments(3*n))
 console.warn("Does there exist a loop that avoids deadlocks?", bdd.and(initial, ctl.EG(bdd.not(deadlocks), transition)).isSatisfiable)
-console.warn("Does there exist a loop that avoids deadlocks, where each philosopher gets to eat infinitely often?", bdd.and(initial, fair_ctl.EG(bdd.not(deadlocks), fairness, transition)).isSatisfiable)
